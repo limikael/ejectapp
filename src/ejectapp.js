0a2363088e2c13a3fd3e60c6e5a8aec20ebc66a9
Mikael Lindqvist <li.mikael@gmail.com>
2014-05-27 11:24:58 +0200

    build ejecta libs

--- src/ejectapp.js
+++ src/ejectapp.js
@@ -5,6 +5,7 @@ var FileUtil = require("./utils/FileUtil");
 var TaskRunner = require("./taskrunner/TaskRunner");
 var DownloadFileTask = require("./tasks/DownloadFileTask");
 var ExtractArchiveTask = require("./tasks/ExtractArchiveTask");
+var JobTask = require("./tasks/JobTask");
 
 /*
  * Set things up.
@@ -13,37 +14,42 @@ function setup() {
 	Fiber(function() {
 		FileUtil.ensureDirectorySync(__dirname + "/../content");
 
-		var runner=new TaskRunner("Enable target: android");
+		var runner = new TaskRunner("Enable target: android");
 		var task;
 
-		task=new DownloadFileTask("Downloading Ejecta-X");
+		task = new DownloadFileTask("Downloading Ejecta-X");
 		task.setSrc("https://github.com/Wizcorp/Ejecta-X/archive/v0.8.0.zip");
 		task.setDest(__dirname + "/../content/Ejecta-X-v0.8.0.zip");
 		runner.addTask(task);
 
-		task=new ExtractArchiveTask("Extracting Ejecta-X");
+		task = new ExtractArchiveTask("Extracting Ejecta-X");
 		task.setSrc(__dirname + "/../content/Ejecta-X-v0.8.0.zip");
 		task.setDest(__dirname + "/../content/Ejecta-X-v0.8.0");
 		runner.addTask(task);
 
-		task=new DownloadFileTask("Downloading Android NDK");
+		task = new DownloadFileTask("Downloading Android NDK");
 		task.setSrc("http://dl.google.com/android/ndk/android-ndk-r9d-darwin-x86_64.tar.bz2");
-		task.setDest(__dirname+"/../content/android-ndk-r9d-darwin-x86_64.tar.bz2");
+		task.setDest(__dirname + "/../content/android-ndk-r9d-darwin-x86_64.tar.bz2");
 		runner.addTask(task);
 
-		task=new ExtractArchiveTask("Extracting Android NDK");
+		task = new ExtractArchiveTask("Extracting Android NDK");
 		task.setSrc(__dirname + "/../content/android-ndk-r9d-darwin-x86_64.tar.bz2");
 		task.setDest(__dirname + "/../content/android-ndk-r9d-darwin-x86_64");
 		runner.addTask(task);
 
-		task=new DownloadFileTask("Downloading Android SDK");
+		task = new DownloadFileTask("Downloading Android SDK");
 		task.setSrc("http://dl.google.com/android/adt/22.6.2/adt-bundle-mac-x86_64-20140321.zip");
 		task.setDest(__dirname + "/../content/adt-bundle-mac-x86_64-20140321.zip");
 		runner.addTask(task);
 
-		task=new ExtractArchiveTask("Extracting Android SDK");
+		task = new ExtractArchiveTask("Extracting Android SDK");
 		task.setSrc(__dirname + "/../content/adt-bundle-mac-x86_64-20140321.zip");
 		task.setDest(__dirname + "/../content/adt-bundle-mac-x86_64-20140321");
+		runner.addTask(task);
+
+		task = new JobTask("Building Ejecta-X libraries");
+		task.setCommand(__dirname + "/../content/android-ndk-r9d-darwin-x86_64/android-ndk-r9d/ndk-build");
+		task.setDir(__dirname + "/../content/Ejecta-X-v0.8.0/Ejecta-X-0.8.0/project/android");
 		runner.addTask(task);
 
 		runner.run();
