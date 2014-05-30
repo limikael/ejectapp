90be618ac82f369f02fae3e16d5718e6c9de8ca4
Mikael Lindqvist <li.mikael@gmail.com>
2014-05-30 15:41:35 +0200

    restart

--- test/downloadtest.js
+++
@@ -1,9 +0,0 @@
-var FileDownloader=require("../src/utils/FileDownloader");
-
-var dl=new FileDownloader();
-//dl.setUrl("https://github.com/Wizcorp/Ejecta-X/archive/v0.8.0.zip");
-dl.setUrl("http://www.dn.se/adsfadsf");
-dl.setTargetFileName("test.zip");
-
-dl.download();
-
